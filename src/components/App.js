bb98080846cc2e14f37510c8e8bc1cff1bf3d517
Detlolka <detlolka@yahoo.com>
2020-08-26 13:37:10 +0300

    feature: сделан PopupForm

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,7 @@ import Footer from "./Footer";
 import PopupWithImage from "./PopupWithImage";
 import PopupWithForm from "./PopupWithForm";
 import CurrentUserContext from "../contexts/CurrentUserContext";
+import EditProfilePopup from "./EditProfilePopup";
 import api from "../utils/api";
 
 class App extends React.Component {
@@ -18,17 +19,17 @@ class App extends React.Component {
       selectedCard: null,
       isDeletePlacePopupOpen: false,
       currentUser: {
-        userName: '',
-        userAbout: '',
-        userAvatar: '',
-        userId: ''
+        userName: "",
+        userAbout: "",
+        userAvatar: "",
+        userId: "",
       },
       cardItems: [],
       isLoadingUpdate: {
         editProfile: false,
         editAvatar: false,
-        createPlace: false
-      }
+        createPlace: false,
+      },
     };
   }
 
@@ -71,80 +72,91 @@ class App extends React.Component {
       isDeletePlacePopupOpen: false,
     });
   };
-  
-//смена данных пользователя
-handleUpdateUser = (name, about) => {  
-  api
-    .changeUserInfo(name, about)
-    .then(user =>
-      this.setState({
-        currentUser: {
-          ...this.state.currentUser,
-          userName: user.name,
-          userDescription: user.about
-        }
-      }))
-    .catch(err => console.error(err))    
-}
 
-//Смена аватара пользователя
-  handleUpdateAvatar = (avatar) => {    
+  //смена данных пользователя
+  handleUpdateUser = (name, about) => {
     api
-      .changeAvatar(avatar)
-      .then(user =>
+      .changeUserInfo(name, about)
+      .then((user) =>
         this.setState({
           currentUser: {
             ...this.state.currentUser,
-            userAvatar: user.avatar
-          }
-        }))
-      .catch(err => console.error(err))
-  }
+            userName: user.name,
+            userDescription: user.about,
+          },
+        })
+      )
+      .catch((err) => console.error(err));
+  };
 
-//Удаление карточки и пересоздание массива
-  handleCardDelete = (cardId) => {                           
-    api.deleteCard(cardId).then((res) => {
-      const newCards = this.state.cardItems.filter((c) => c._id !== cardId);
-      this.setState({
-        cardItems: newCards
-      });
-    })
-      .catch(err => console.error(err));
-  }
+  //Смена аватара пользователя
+  handleUpdateAvatar = (avatar) => {
+    api
+      .changeAvatar(avatar)
+      .then((user) =>
+        this.setState({
+          currentUser: {
+            ...this.state.currentUser,
+            userAvatar: user.avatar,
+          },
+        })
+      )
+      .catch((err) => console.error(err));
+  };
 
-//Постановка лайка и удаление лайка
+  //Удаление карточки и пересоздание массива
+  handleCardDelete = (cardId) => {
+    api
+      .deleteCard(cardId)
+      .then((res) => {
+        const newCards = this.state.cardItems.filter((c) => c._id !== cardId);
+        this.setState({
+          cardItems: newCards,
+        });
+      })
+      .catch((err) => console.error(err));
+  };
+
+  //Постановка лайка и удаление лайка
   changeCardLike = (card) => {
-    const isLiked = card.likes.some(i => i._id === this.state.currentUser.userId);
-    api.likeCardStatus(card._id, !isLiked).then((newCard) => {
-      const newCards = this.state.cards.map((c) => c._id === card._id ? newCard : c);
-      this.setState({
-        cardItems: newCards
-      });
-    })
-      .catch(err => console.error(err));
-  }
+    const isLiked = card.likes.some(
+      (i) => i._id === this.state.currentUser.userId
+    );
+    api
+      .likeCardStatus(card._id, !isLiked)
+      .then((newCard) => {
+        const newCards = this.state.cards.map((c) =>
+          c._id === card._id ? newCard : c
+        );
+        this.setState({
+          cardItems: newCards,
+        });
+      })
+      .catch((err) => console.error(err));
+  };
 
-//Добавление новой карточки
-  handleAddPlaceSubmit = (name, link) => {                        
-    api.createCard(name, link)
+  //Добавление новой карточки
+  handleAddPlaceSubmit = (name, link) => {
+    api
+      .createCard(name, link)
       .then((newCard) => {
         this.setState({
-          cardItems: [...this.state.cardItems, newCard]
-        })
+          cardItems: [...this.state.cardItems, newCard],
+        });
       })
-      .catch(err => console.error(err))      
-  }
+      .catch((err) => console.error(err));
+  };
 
- //Перенесен Api в App
-  componentDidMount() {                                        
+  //Перенесен Api в App
+  componentDidMount() {
     Promise.all([api.getInitialCards(), api.getUserInfo()])
       .then(([cardItems, user]) => {
         this.setState({
           currentUser: {
-          userName: user.name,
-          userAbout: user.about,
-          userAvatar: user.avatar,
-          userId: user.id,          
+            userName: user.name,
+            userAbout: user.about,
+            userAvatar: user.avatar,
+            userId: user.id,
           },
           cardItems,
         });
@@ -154,124 +166,96 @@ handleUpdateUser = (name, about) => {
 
   render() {
     return (
-      <div className="page">
-        <Header />
-        <Main
-          onEditProfile={this.handleEditProfileClick}
-          onEditPlace={this.handleEditPlaceClick}
-          onEditImage={this.handleEditImageClick}
-          onDeletePlace={this.handleEditDeleteClick}
-          onEditAvatar={this.handleEditAvatarClick}
-        />
-        <Footer />
-        <PopupWithForm
-          name="delete-place"
-          title="Вы уверены?"
-          buttonName="Да"
-          isOpen={this.state.isDeletePlacePopupOpen}
-          onClose={this.closeAllPopups}
-        />
-        <PopupWithForm
-          name="profile"
-          title="Редактировать профиль"
-          buttonName="Сохранить"
-          isOpen={this.state.isEditProfilePopupOpen}
-          onClose={this.closeAllPopups}
-          children={
-            <React.Fragment>
-              <input
-                type="text"
-                name="profileName"
-                defaultValue="Жак-Ив Кусто"
-                className="popup__input popup__input_name"
-                id="input-name"
-                placeholder="Имя"
-                required
-                minLength="2"
-                maxLength="40"
-                pattern="[А-ЯЁа-яёA-Za-z-\s]*"
-              />
-              <span className="error" id="input-name-error" />
+      <CurrentUserContext.Provider value={this.state.currentUser}>
+        <div className="page">
+          <Header />
+          <Main
+            onEditProfile={this.handleEditProfileClick}
+            onEditPlace={this.handleEditPlaceClick}
+            onEditImage={this.handleEditImageClick}
+            onDeletePlace={this.handleEditDeleteClick}
+            onEditAvatar={this.handleEditAvatarClick}
+          />
+          <Footer />
+          <PopupWithForm
+            name="delete-place"
+            title="Вы уверены?"
+            buttonName="Да"
+            isOpen={this.state.isDeletePlacePopupOpen}
+            onClose={this.closeAllPopups}
+          />
+          <EditProfilePopup            
+            isOpen={this.state.isEditProfilePopupOpen}
+            onClose={this.closeAllPopups}
+            onUpdateUser={(name, about) =>
+              this.handleUpdateUser(name, about)
+            }
+          />
 
-              <input
-                type="text"
-                name="profileAbout"
-                defaultValue="Исследователь океана"
-                className="popup__input popup__input_about"
-                id="input-about"
-                placeholder="О себе"
-                required
-                minLength="2"
-                maxLength="200"
-              />
-              <span className="error" id="input-about-error" />
-            </React.Fragment>
-          }
-        />
+          <PopupWithForm
+            name="card"
+            title="Новое место"
+            buttonName="Создать"
+            isOpen={this.state.isAddPlacePopupOpen}
+            onClose={this.closeAllPopups}
+            children={
+              <React.Fragment>
+                <input
+                  type="text"
+                  name="placeName"
+                  defaultValue=""
+                  className="popup__input popup__input_place"
+                  id="input-place"
+                  placeholder="Название"
+                  required
+                  minLength="1"
+                  maxLength="30"
+                  pattern="[А-ЯЁа-яёA-Za-z-\s]*"
+                />
+                <span className="error" id="input-place-error" />
 
-        <PopupWithForm
-          name="card"
-          title="Новое место"
-          buttonName="Создать"
-          isOpen={this.state.isAddPlacePopupOpen}
-          onClose={this.closeAllPopups}
-          children={
-            <React.Fragment>
-              <input
-                type="text"
-                name="placeName"
-                defaultValue=""
-                className="popup__input popup__input_place"
-                id="input-place"
-                placeholder="Название"
-                required
-                minLength="1"
-                maxLength="30"
-                pattern="[А-ЯЁа-яёA-Za-z-\s]*"
-              />
-              <span className="error" id="input-place-error" />
+                <input
+                  type="url"
+                  name="placePhoto"
+                  defaultValue=""
+                  className="popup__input popup__input_image"
+                  id="input-url"
+                  placeholder="Ссылка на картинку"
+                  required
+                />
+                <span className="error" id="input-url-error" />
+              </React.Fragment>
+            }
+          />
 
-              <input
-                type="url"
-                name="placePhoto"
-                defaultValue=""
-                className="popup__input popup__input_image"
-                id="input-url"
-                placeholder="Ссылка на картинку"
-                required
-              />
-              <span className="error" id="input-url-error" />
-            </React.Fragment>
-          }
-        />
+          <PopupWithForm
+            name="avatar"
+            title="Обновить аватар"
+            buttonName="Сохранить"
+            isOpen={this.state.isEditAvatarPopupOpen}
+            onClose={this.closeAllPopups}
+            children={
+              <React.Fragment>
+                <input
+                  type="url"
+                  name="avatarPhoto"
+                  defaultValue=""
+                  className="popup__input popup__input_avatar"
+                  id="input-avatar"
+                  placeholder="Введите URL"
+                  required
+                />
+                <span className="error" id="input-avatar-error" />
+              </React.Fragment>
+            }
+          />
 
-        <PopupWithForm
-          name="avatar"
-          title="Обновить аватар"
-          buttonName="Сохранить"
-          isOpen={this.state.isEditAvatarPopupOpen}
-          onClose={this.closeAllPopups}
-          children={
-            <React.Fragment>
-              <input
-                type="url"
-                name="avatarPhoto"
-                defaultValue=""
-                className="popup__input popup__input_avatar"
-                id="input-avatar"
-                placeholder="Введите URL"
-                required
-              />
-              <span className="error" id="input-avatar-error" />
-            </React.Fragment>
-          }
-        />
-
-        <PopupWithImage
-          card={this.state.selectedCard}
-          onClose={this.closeAllPopups}
-        />
-      </div>
+          <PopupWithImage
+            card={this.state.selectedCard}
+            onClose={this.closeAllPopups}
+          />
+        </div>
+      </CurrentUserContext.Provider>
     );
   }
 }
